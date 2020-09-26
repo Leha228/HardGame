2e19b7d52797267333f93503ece5a967340f9e81
SSaNyAS <61096135+SSaNyAS@users.noreply.github.com>
2020-09-26 21:13:37 +0500

    Создал ветку

---
+++ Assets/Scripts/IMove.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface IMove
+{
+   enum Directions : UInt16
+    {
+        Up,
+        Left,
+        Down,
+        Right
+    }
+    public void MoveTo(Transform position);
+}
