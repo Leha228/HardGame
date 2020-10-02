c360e57b2e3f3e93ab9416492f31af49d759f476
SSaNyAS <61096135+SSaNyAS@users.noreply.github.com>
2020-10-03 01:20:31 +0500

    Добавил события о изменении данных игрока, изменил функции получения урона и пополения хп, сделал нормальное визуальное отображение параметров игрока в панели компонентов

---
+++ Assets/Scripts/EventDelegates.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class EventDelegates
+{
+    // IHandlingAliveObject
+    public delegate void onHealthChanged(float newValue);
+    public delegate void onMoveSpeedChanged(float newValue);
+    public delegate void onDeath();
+    public delegate void onHealing(float newHealthValue);
+    public delegate void onAcceptDamage(float damage);
+}
