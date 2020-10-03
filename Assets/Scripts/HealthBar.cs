03ef280bc776bc9bf1214677872b4196e4f467b3
SSaNyAS <61096135+SSaNyAS@users.noreply.github.com>
2020-10-03 20:55:55 +0500

    Добавил тестовые отображение здоровья и таймер в котором каждые 2 секунды вычитается 1хп

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private AliveObject Player = null;
+    [SerializeField] private Text TextField = null;
+    private Timer timer = new Timer(2000);
+    void Start()
+    {
+        Player.OnHealthChangedEvent += Player_OnHealthChangedEvent;
+        timer.Elapsed += Timer_Elapsed;
+        timer.Start();
+    }
+
+    private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+    {
+        print("timerTick");
+        Player.OnAcceptDamage(1);
+    }
+
+    private void Player_OnHealthChangedEvent(float newValue)
+    {
+        print("Health Changed");
+        this.TextField.text = $"Health: {newValue}";
+        this.TextField.LayoutComplete();
+    }
+}
